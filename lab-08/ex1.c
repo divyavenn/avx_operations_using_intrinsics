698f486f8c41802e94fb1f5b44fdc51c3104ca2d
divya <divyavenkat@gmail.com>
2020-11-13 01:35:50 -0500

    done!

--- lab-08/ex1.c
+++ lab-08/ex1.c
@@ -17,10 +17,26 @@ void print__m256(__m256 data){
 	float*f = (float*)&data;
 	printf("%f %f %f %f %f %f %f %f\n", f[0],f[1],f[2],f[3],f[4],f[5],f[6],f[7]);
 }
-
+void doTheThing(float i){
+	__m256 data = _mm256_set1_ps(i);
+	__m256 x = _mm256_set1_ps (1);
+	data =  _mm256_add_ps (x, data);
+	float* toPrint = (float*)&data;
+	printf("%i ", (int)(toPrint[0]));	
+}
 int main(){
-
-
-
+	FILE *fp;
+	fp = fopen("data.txt", "r");
+	if (fp == NULL){
+		printf("Can't find file!");
+		exit(1);
+	}
+	int value;
+	float num;
+	while (fscanf(fp, "%f", &num) != EOF){
+		//Zhu L
+		doTheThing(num);
+	}	
+	printf("\n");
 	return 0;
 }
--- lab-08/ex2.c
+++ lab-08/ex2.c
@@ -1,3 +1,4 @@
+// For this exercise, you are going to read in data from a file called 'data.txt'
 // Simple Caeser Shift decrypt
 // 
 // Load the data from data2.txt. Increment all of the values by 1 then output
@@ -8,21 +9,46 @@
 //
 // Run as normal with: 
 // ./ex2prog
-//
 #include <stdio.h>
 #include <immintrin.h>
-
+#include <string.h>
 
 
 // print (Note, you may need to change this function or write another for your data)
-void print__m256(__m256 data){
-	float*f = (float*)&data;
-	printf("%f %f %f %f %f %f %f %f\n", f[0],f[1],f[2],f[3],f[4],f[5],f[6],f[7]);
+void doTheThing(int asc){
+	__m256i data = _mm256_set1_epi8(asc);
+	__m256i x = _mm256_set1_epi8(1);
+	data =  _mm256_add_epi8 (x, data);
+	char* toPrint = (char*)&data;
+	printf("%c ", (toPrint[0]));	
 }
-
 int main(){
-
-
-
+	FILE *fp;
+	fp = fopen("data2.txt", "r");
+	if (fp == NULL){
+		printf("Can't find file!");
+		exit(1);
+	}
+	int value;
+	char ch[150];
+	while (fgets(ch, 150, fp) != NULL){
+		char * c = strtok(ch, "\n");
+		int i;
+		int j;
+		int asc = 0;
+		int digit = 0;
+		int len = strlen(c);
+		for (int i = 0; i<len; i++){
+			digit = ((int)(c[i]) - 48);
+			for (int j = 0; j < (len - i - 1); j++){
+				digit = digit * 10;
+			}
+			asc += digit;
+		}
+		//printf("%i ", asc);
+		doTheThing(asc);
+	}	
+	//doTheThing(101);
+	printf("\n");
 	return 0;
 }
